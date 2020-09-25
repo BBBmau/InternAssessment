6eedb9431d35bacdc8e8681f176eaea7ca1f4269
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2020-09-25 14:09:55 -0700

    Update popularWord.cpp
    Added the condition where if their are multiple words with the same frequency it marks it as not popular and returns the string, "No Most Popular Word Exists."

--- popularWord.cpp
+++ popularWord.cpp
@@ -1,7 +1,8 @@
 string popular(string paragraph) {
+	bool multiple = false;
 	vector<string> set;
 	string word = "";
-	int counter = 0;
+	int counter;
 	int most = 0;
 	int i, j;
 	string top;
@@ -17,10 +18,8 @@ string popular(string paragraph) {
 		}
 		word.push_back(paragraph[i]);
 	}
-
-
 	for (i = 0; i < set.size(); i++) {
-		counter = 0;
+		counter = 1;
 		for (j = 0; j < set.size(); j++) {
 			if (i == j) {
 				continue;
@@ -29,10 +28,19 @@ string popular(string paragraph) {
 				counter++;
 			}
 		}
-		if (counter > most) {
+		if ((counter >= most) && (top != set[i])) {
+			if (counter == most) {
+				multiple = true;
+			}
+			else if (counter > most) {
+				multiple = false;
+			}
 			top = set[i];
 			most = counter;
 		}
+	}
+	if (multiple) {
+		top = "No Most Popular Word Exists.";
 	}
 	return top;
 }
