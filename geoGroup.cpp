67fc50c58963bd27065a368f996589472373ef52
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2020-09-26 00:33:03 -0700

    Create geoGroup.cpp
    Sorts the People Objects Based on Location such as State and City

---
+++ geoGroup.cpp
@@ -0,0 +1,36 @@
+string geoGroup(People Group[]) {
+    string temp;
+    vector<string> Cities;
+
+    for (int i = 0; i < sizeof(Group); i++) {
+        Cities.push_back(temp.assign(Group[i].Address.begin() + ((Group[i].Address).find(",")) + 2, (Group[i].Address).end()));
+    }
+
+    sort(Cities.begin(), Cities.end());
+    auto it = unique(Cities.begin(), Cities.end());
+    Cities.erase(it, Cities.end());
+
+    vector<vector<string>> Pop;
+
+    for (int i = 0; i < Cities.size(); i++){
+        vector<string> Row;
+        Row.push_back(Cities[i]);
+        for (int j = 0; j < sizeof(Group); j++) {
+            if ((Group[j].Address).find(temp.assign(Group[i].Address.begin() + ((Group[i].Address).find(",")) + 2, (Group[i].Address).end())) != -1) {
+                Row.push_back(Group[j].name);
+            }
+        }
+        Pop.push_back(Row);
+    }
+
+    for (int i = 0; i < Pop.size(); i++) {
+        for (int j = 0; j < Pop[i].size(); j++) {
+            cout << Pop[i][j] << endl;
+            if (j == 0) {
+                cout << "==============" << endl;
+            }
+        }
+        cout << endl;
+    }
+    return "";
+}
