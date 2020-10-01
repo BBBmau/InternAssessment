3b530be609a557dff8e46115b436a756e9a665c6
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2020-10-01 01:20:31 -0700

    Final Solution
    Added comments explaining how the code works

--- Kotlin/reverseWord.kt
+++ Kotlin/reverseWord.kt
@@ -1,6 +1,7 @@
 fun reverseWord(original: String): String{
 
     var reverseWord = ""
+        // Starts for loop at the end of the string in order to grab character and store at the front of string "reverse"
             for(x in original.length - 1 downTo 0){
                 reverseWord += original[x]
 
