3d9c0af35d374a564bc8729f9324a8668048c2d0
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2020-10-01 00:19:30 -0700

    Final Solution
    added comments explaining the code that was written. Added the headers used. Added how the People class is setup in order to test out the geoCoordinate Function.

--- C++/geoCoordinate.cpp
+++ C++/geoCoordinate.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// class used to test geoCoordinate function
+class People {
+    public:
+    string Birthday;
+    string name;
+    double Location[2];
+    string Address;
+};
+
 string geoCoordinate(People Person, People Group[]){
     double Lat1 = Person.Location[0];
     double Lon1 = Person.Location[1];
@@ -6,21 +21,28 @@ string geoCoordinate(People Person, People Group[]){
     double closest = 6371;
     string closestPerson;
 
+    
     for (int i = 0; i < sizeof(Group); i++) {
+        // converts to radians
         double dlat = (((Group[i].Location)[0] - Lat1) * PI) / 180;
         double dlon = (((Group[i].Location)[1] - Lon1) * PI) / 180;
 
+        // Using the Haversine Formula we are able to calculate the distance from one person to another with coordinates
         double a = sin(dlat / 2) * sin(dlat / 2) + cos((Lat1 * PI) / 180) * cos((((Group[i].Location)[0]) * PI)/ 180)
             * sin(dlon / 2) * sin(dlon / 2);
         double c = 2 * atan2(sqrt(a), sqrt(1 - a));
         
         double d = radius * c;
 
+        // Goes Through Group array to find which person is the closest
         if (d < closest) {
             closest = d;
             closestPerson = Group[i].name;
         }
     }
-    cout << closest << endl;
+    // Distance that is calculated would be in km
+    //cout << closest << endl;
+    
+    
     return closestPerson;
 }
