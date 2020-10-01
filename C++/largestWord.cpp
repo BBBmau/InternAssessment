04a0af4d801c13237ceca8ca9e96fedc262caee3
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2020-10-01 00:57:22 -0700

    Final Solution
    Added Headers and Comments explaining lines of Code.

--- C++/largestWord.cpp
+++ C++/largestWord.cpp
@@ -1,12 +1,19 @@
+#include <iostream>
+#include <string>
+
 string largest(string paragraph) {
 	string largest = "";
 	string word = "";
 
 	for (int i = 0; i < paragraph.length(); i++) {
+		//Checks whether character is a letter, if it is it stores it in the string "word"
 		if (!isalpha(paragraph[i])) {
+			//Once character is found to not be a letter, such as whitespace or number, it checks whether or not
+			// it is bigger than the previous stored word in "largest"
 			if (word.length() > largest.length()) {
 				largest = word;	
 			}
+			//resets word string
 			word = "";
 			continue;
 		}
