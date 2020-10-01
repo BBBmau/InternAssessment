e9809ed5fbc31558684405953a4a28267e66c00d
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2020-10-01 00:59:28 -0700

    Final Solution
    Added Headers and Comments explaining code.

--- C++/reverseOrder.cpp
+++ C++/reverseOrder.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
+#include <string>
+
 string reverse(string const original) {
 
 	string reverse;
-
+	
+	// Starts for loop at the end of the string in order to grab character and store at the front of string "reverse"
 	for (int i = original.length(); i >= 0; i--) {
 		reverse.push_back(original[i]);
 	}
 	return reverse;
-
 }
