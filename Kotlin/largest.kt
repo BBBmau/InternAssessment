c1026f9e011c1fc49ac525c00ea70feb767728ad
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2020-10-01 01:14:05 -0700

    Final Solution
    Added Comments explaining how the code works

--- Kotlin/largest.kt
+++ Kotlin/largest.kt
@@ -1,15 +1,21 @@
 fun largest(paragraph: String): String{
     var largest = ""
     var word = ""
-
+    
+    // loops for the amount of characters exist in the paragraph string
     for (i in paragraph.indices){
+        // statement checks whether character is a letter
         if (!paragraph[i].isLetter()){
+            // if met, it checks whether the word string that is used to store each word in the string is greater than the previously
+            // stored word that is in the string "largest"
             if(word.length > largest.length){
                 largest = word
             }
+            // resets the word string
             word = ""
             continue
         }
+        // adds character to the word string
         word += paragraph[i]
     }
     return largest
