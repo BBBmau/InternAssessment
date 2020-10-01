475154b74d8595fefa3b52d82033213f779acf71
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2020-10-01 01:10:38 -0700

    Final Solution
    Added Comments explaining how the code works.

--- Kotlin/dateChecker.kt
+++ Kotlin/dateChecker.kt
@@ -2,8 +2,13 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 fun dateChecker(dateInput: String):String{
+    // using the java time to get today's date
     val today = LocalDateTime.now()
+    
+    //sets the format of how we want the date
     val monthFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+    
+    //statement checks whether the date inputted is the same as today's date
     if(today.format(monthFormat) == dateInput){
         return "The Date entered is today!"
     }else{
